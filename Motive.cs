4bf55c6f1f9c329d962881547a067d914d1af8e3
Ava Pek <exodrifter@gmail.com>
2018-05-09 19:08:33 -0500

    Add mouse tracker

---
+++ Motive.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+
+namespace Exodrifter.Motive
+{
+	/// <summary>
+	/// A helper for the motive class to keep track of input data.
+	/// </summary>
+	[AddComponentMenu("")]
+	internal class Motive : MonoBehaviour
+	{
+		#region Data
+
+		/// <summary>
+		/// Mouse tracking data.
+		/// </summary>
+		public static MouseTracker Mouse
+		{
+			get
+			{
+				mouse = mouse ?? new MouseTracker(Input.mousePosition);
+				return mouse;
+			}
+		}
+		private static MouseTracker mouse;
+
+		#endregion
+
+		#region Monobehaviour
+
+		private void Update()
+		{
+			Mouse.Update();
+		}
+
+		#endregion
+
+		#region Singleton
+
+		private static Motive Instance
+		{
+			get
+			{
+				Init();
+				return instance;
+			}
+		}
+		private static Motive instance = null;
+		private static bool quitting = false;
+
+		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+		private static void Init()
+		{
+			// Check if the tracker has already been initialized
+			if (instance != null && !instance.Equals(null))
+			{
+				return;
+			}
+
+			// Don't initialize if the application is quitting
+			if (quitting)
+			{
+				return;
+			}
+
+			var gameObject = new GameObject("Motive");
+			gameObject.hideFlags = HideFlags.HideAndDontSave;
+			DontDestroyOnLoad(gameObject);
+
+			instance = gameObject.AddComponent<Motive>();
+		}
+
+		private void Awake()
+		{
+			// Check if another instance of the tracker already exists
+			if (instance != null && !instance.Equals(null))
+			{
+				if (this != instance)
+				{
+					Destroy(gameObject);
+				}
+			}
+		}
+
+		private void OnApplicationQuit()
+		{
+			quitting = true;
+		}
+
+		private void OnDestroy()
+		{
+			if (this == instance)
+			{
+				instance = null;
+			}
+
+			// Re-initialize the tracker
+			Init();
+		}
+
+		#endregion
+	}
+}
---
+++ MouseTracker.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+namespace Exodrifter.Motive
+{
+	/// <summary>
+	/// The mouse tracker stores and scrapes data related to Mouse Input from
+	/// the Unity API.
+	/// </summary>
+	public class MouseTracker
+	{
+		/// <summary>
+		/// The position of the mouse this frame.
+		/// </summary>
+		public Vector3 Position
+		{
+			get { return position; }
+		}
+		private Vector3 position;
+
+		/// <summary>
+		/// The position of the mouse last frame.
+		/// </summary>
+		public Vector3 LastPosition
+		{
+			get { return lastPosition; }
+		}
+		private Vector3 lastPosition;
+
+		/// <summary>
+		/// The change in mouse position since the last frame.
+		/// </summary>
+		public Vector3 Delta
+		{
+			get { return delta; }
+		}
+		private Vector3 delta = Vector3.zero;
+
+		/// <summary>
+		/// Creates a new mouse tracker.
+		/// </summary>
+		internal MouseTracker(Vector3 mousePosition)
+		{
+			position = mousePosition;
+			lastPosition = mousePosition;
+		}
+
+		/// <summary>
+		/// Updates the state of the tracker.
+		/// </summary>
+		internal void Update()
+		{
+			lastPosition = position;
+			position = Input.mousePosition;
+			delta = position - lastPosition;
+		}
+	}
+}
